f109b49ddc166a1cfd657988daad75bc767507b5
Nahuel Garrido <garridonahuel8@gmail.com>
2023-08-07 02:25:31 -0300

    send dynamic active user id

--- src/app/views/home/home.component.ts
+++ src/app/views/home/home.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { TransactionService } from 'src/app/services/transaction.service';
 import { Transaction } from 'src/app/models/transaction.model';
+import { AuthenticationService } from 'src/app/services/authentication.service';
 
 @Component({
   selector: 'app-home',
@@ -9,7 +10,10 @@ import { Transaction } from 'src/app/models/transaction.model';
 })
 export class HomeComponent implements OnInit {
   transactions: Transaction[] = [];
-  constructor(private _transactionService: TransactionService) {}
+  constructor(
+    private _transactionService: TransactionService,
+    private authService: AuthenticationService
+  ) {}
 
   ngOnInit(): void {
     console.log('hi');
@@ -17,8 +21,9 @@ export class HomeComponent implements OnInit {
   }
 
   updateTransactions() {
+    const activeUserID = this.authService.getActiveUserId() as string;
     this._transactionService
-      .getTransactionsByUserID('30111222')
+      .getTransactionsByUserID(activeUserID)
       .subscribe((transactions) => {
         this.transactions = transactions;
       });
