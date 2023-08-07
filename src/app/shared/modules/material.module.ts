1fe6e140dcc7b4c64fb789a55f88a9a789e769d7
Nahuel Garrido <garridonahuel8@gmail.com>
2023-08-07 03:12:32 -0300

    implement transactions table

--- src/app/shared/modules/material.module.ts
+++ src/app/shared/modules/material.module.ts
@@ -3,6 +3,8 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatSelectModule } from '@angular/material/select';
+import { MatTableModule } from '@angular/material/table';
+import { MatPaginatorModule } from '@angular/material/paginator';
 
 @NgModule({
   imports: [
@@ -10,12 +12,16 @@ import { MatSelectModule } from '@angular/material/select';
     MatSelectModule,
     MatInputModule,
     MatButtonModule,
+    MatTableModule,
+    MatPaginatorModule,
   ],
   exports: [
     MatFormFieldModule,
     MatSelectModule,
     MatInputModule,
     MatButtonModule,
+    MatTableModule,
+    MatPaginatorModule,
   ],
 })
 export class MaterialModule {}
